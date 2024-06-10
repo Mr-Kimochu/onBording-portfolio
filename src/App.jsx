cc1052cf0ced7fad094bad6eeedffe2c069d6ab2
Mr-Kimochu <126611224+Mr-Kimochu@users.noreply.github.com>
2024-06-10 09:37:36 +0000

    add all changes

--- src/App.jsx
+++ src/App.jsx
@@ -24,14 +24,14 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
+  name: "George Kimani",
+  title: "Backend Developer & UI/UX Designer",
+  email: "george2@kim.com",
+  gitHub: "https://github.com/Mr-Kimochu",
   instagram: "microsoft",
-  linkedIn: "satyanadella",
+  linkedIn: "https://www.linkedin.com/in/george-kimochu-a8b702269/",
   medium: "",
-  twitter: "microsoft",
+  twitter: "kimaniiii0",
   youTube: "Code",
 };
 
